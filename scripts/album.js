5cb47f305a1123e51423a879b9a55cf84c4811e1
Melrieoi <melroy.tygo.loen@gmail.com>
2025-03-09 22:33:46 +0100

    test for listing names on bands page

---
+++ scripts/album.js
@@ -0,0 +1,25 @@
+const urlParams = new URLSearchParams(window.location.search);
+const band = urlParams.get("band");
+const album = urlParams.get("album");
+
+fetch(`../Bands/${band}.json`)
+  .then(response => response.json())
+  .then(data => {
+      let albumData = data.albums.find(a => a.albumName === album);
+      document.getElementById("albumTitle").innerText = `${albumData.albumName} (${albumData.year})`;
+      
+      let songsContainer = document.getElementById("songsContainer");
+      albumData.songs.forEach(song => {
+          let div = document.createElement("div");
+          div.innerHTML = `
+              <a href="#" onclick="showLyrics('${song.songName}', '${song.lyrics.replace(/\n/g, "<br>")}')">${song.songName}</a>
+          `;
+          songsContainer.appendChild(div);
+      });
+  });
+
+function showLyrics(title, lyrics) {
+    document.getElementById("lyricsModalTitle").innerText = title;
+    document.getElementById("lyricsModalBody").innerHTML = lyrics;
+    document.getElementById("lyricsModal").style.display = "block";
+}
---
+++ scripts/band.js
@@ -0,0 +1,18 @@
+const urlParams = new URLSearchParams(window.location.search);
+const band = urlParams.get("band");
+
+fetch(`../Bands/${band}.json`)
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById("bandTitle").innerText = data.bandName;
+      let albumsContainer = document.getElementById("albumsContainer");
+      
+      data.albums.forEach(album => {
+          let div = document.createElement("div");
+          div.innerHTML = `
+              <img src="../${album.cover}">
+              <a href="../Albums/album.html?band=${band}&album=${album.albumName}">${album.albumName} (${album.year})</a>
+          `;
+          albumsContainer.appendChild(div);
+      });
+  });
