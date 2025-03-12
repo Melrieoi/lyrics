7682c7ec4935d027bc69708e96b31088a6968a88
Melrieoi <melroy.tygo.loen@gmail.com>
2025-03-12 15:38:35 +0100

    test band.html and band.js

--- scripts/band.js
+++ scripts/band.js
@@ -3,17 +3,25 @@ const urlParams = new URLSearchParams(window.location.search);
 const band = urlParams.get("band");
 
 fetch(`../lyrics/Bands/${band}.json`)
-  .then(response => response.json())
-  .then(data => {
-      document.getElementById("bandTitle").innerText = data.bandName;
-      let albumsContainer = document.getElementById("albumsContainer");
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("bandTitle").innerText = data.bandName;
+        let albumsContainer = document.getElementById("album");
       
-      data.albums.forEach(album => {
-          let div = document.createElement("div");
-          div.innerHTML = `
-              <img src="../${album.cover}">
-              <a href="album.html?band=${band}&album=${album.albumName}">${album.albumName} (${album.year})</a>
-          `;
-          albumsContainer.appendChild(div);
-      });
-  });
+        data.albums.forEach(album => {
+            let ul = document.createElement("ul");
+            ul.innerHTML = `
+                <li id="albumName"><a href="album.html?band=${band}&album=${album.albumName}">${album.albumName} (${album.year})</a></li>
+                `;
+            albumsContainer.appendChild(ul);
+
+            let albumData = data.albums.find(a => a.albumName === album);
+            albumData.songs.forEach(song => {
+                let li = document.createElement("li");
+                li.innerHTML = `${song.songName}`;
+
+                ul.appendChild(li);
+            });
+        });
+    });
+// <img src="../${album.cover}">
