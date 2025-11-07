9b1236f695ec9f4ffe6d2248e8897c4b16e45ccf
Melrieoi <46975288+Melrieoi@users.noreply.github.com>
2025-11-07 02:10:49 +0100

    Update band.js

--- scripts/band.js
+++ scripts/band.js
@@ -2,7 +2,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const band = urlParams.get("band");
 
-fetch(`../Bands/${band}.json`)
+fetch(`../lyrics/Bands/${band}.json`)
     .then(response => response.json())
     .then(data => {
         document.getElementById("pageTitle").innerText = data.bandName; // Sets the page title
